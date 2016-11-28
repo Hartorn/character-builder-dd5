33c75f107adfbd7caa23858d6c667f18b6c13485
Hartorn <bazirehoussin@gmail.com>
2016-11-29 00:41:36 +0100

    Migrating from bit balloon to gh-pages

--- deploy.js
+++
@@ -1,8 +0,0 @@
-const bitballoon = require('bitballoon');
-
-bitballoon.deploy({ access_token: process.env.BITBALLOON_ACCESS_TOKEN, site_id: 'd9452aaa-ecaf-4605-93a5-eec17e9a548f', dir: './production' }, function deployCb(err) {
-    if (err) {
-        return console.log('Error deploying');
-    }
-    console.log('New deploy is live');
-});
