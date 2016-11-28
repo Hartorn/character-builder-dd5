6f385d7bcc35a3a45bda5fa22d5613cabc44a939
Hartorn <bazirehoussin@gmail.com>
2016-11-28 16:50:30 +0100

    Correcting deploy

--- deploy.js
+++ deploy.js
@@ -1,21 +1,8 @@
 const bitballoon = require('bitballoon');
-const secrets = require('./bitballoon-secret');
 
-const client = bitballoon.createClient({ client_id: secrets.clientKey, client_secret: secrets.clientSecret });
-client.authorizeFromCredentials(function authentificateCb(err, accessTokenParam) {
+bitballoon.deploy({ access_token: process.env.BITBALLOON_ACCESS_TOKEN, site_id: 'd9452aaa-ecaf-4605-93a5-eec17e9a548f', dir: './production' }, function deployCb(err) {
     if (err) {
-        return console.log(err);
+        return console.log('Error deploying');
     }
-
-    /* client.sites(function logSites(err, sites) {
-         console.log('Sites:' + sites.map(item => console.log(item)));
-     });*/
-    bitballoon.deploy({ access_token: accessTokenParam, site_id: 'd9452aaa-ecaf-4605-93a5-eec17e9a548f', dir: './production' }, function deployCb(err, deploy) {
-        if (err) {
-            return console.log('Error deploying :', err);
-        }
-        console.log('New deploy is live:', deploy);
-    });
+    console.log('New deploy is live');
 });
-
-
